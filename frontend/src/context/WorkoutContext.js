940f456186b47e726ed8951817c783a41dbe5059
marik <gaurav.marik@gmail.com>
2022-07-18 16:43:32 -0400

    WorkoutContext

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -3,28 +3,28 @@ import { createContext, useReducer } from 'react';
 export const WorkoutContext = createContext();
 
 export const workoutReducer = (state, action) => {
-switch(action.type){
-	case 'SET_WORKOUTS':
-		return {
-			workouts: action.payload
-		}
-		case 'CREATE_WORKOUTS':
+	switch (action.type) {
+		case 'SET_WORKOUTS':
 			return {
-				workouts: [action.payload, ...state.workouts]
-			}
-		default: 
-			return state
-}
-}
+				workouts: action.payload,
+			};
+		case 'CREATE_WORKOUT':
+			return {
+				workouts: [action.payload, ...state.workouts],
+			};
+		default:
+			return state;
+	}
+};
 export const WorkoutContextProvider = ({ children }) => {
-const [state, dispatch] = useReducer(workoutReducer, {
-	workouts: null
-})
+	const [state, dispatch] = useReducer(workoutReducer, {
+		workouts: null,
+	});
 
-// dispatch({type:'SET_WORKOUTS', payload:[{}, {}]})
+	// dispatch({type:'SET_WORKOUTS', payload:[{}, {}]})
 
 	return (
-		<WorkoutContext.Provider value={}>
+		<WorkoutContext.Provider value={{ ...state, dispatch }}>
 			{children}
 		</WorkoutContext.Provider>
 	);
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-	const [workouts, setWorkouts] = useState(null);
+	// const [workouts, setWorkouts] = useState(null);
+	const { workouts, dispatch } = useWorkoutsContext();
 
 	useEffect(() => {
 		const fetchWorkouts = async () => {
@@ -13,7 +15,8 @@ const Home = () => {
 			const json = await response.json();
 
 			if (response.ok) {
-				setWorkouts(json);
+				// setWorkouts(json);
+				dispatch({ type: 'SET_WORKOUTS', payload: json });
 			}
 		};
 
