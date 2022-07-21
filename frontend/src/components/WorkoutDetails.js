380dcaf5782f54dcef90c40c1fde687a8d39eadb
marik <gaurav.marik@gmail.com>
2022-07-20 23:21:37 -0400

    material-symbols-outlined & date-fns

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,22 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
 const WorkoutDetails = ({ workout }) => {
+	const { dispatch } = useWorkoutsContext();
+
+	const handleClick = async () => {
+		const response = await fetch('/api/workouts/' + workout._id, {
+			method: 'DELETE',
+		});
+		const json = await response.json();
+
+		if (response.ok) {
+			dispatch({ type: 'DELETE_WORKOUT', payload: json });
+		}
+	};
+
 	return (
 		<div className="workout-details">
 			<h4>{workout.title}</h4>
@@ -10,7 +28,12 @@ const WorkoutDetails = ({ workout }) => {
 				<strong>Reps: </strong>
 				{workout.reps}
 			</p>
-			<p>{workout.createdAt}</p>
+			<p>
+				{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+			</p>
+			<span className="material-symbols-outlined" onClick={handleClick}>
+				delete
+			</span>
 		</div>
 	);
 };
